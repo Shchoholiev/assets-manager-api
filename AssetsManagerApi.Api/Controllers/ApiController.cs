2adbb9423dbcdc5eb36848d8568e2db8013deba4
Shchoholiev <sh.codework@gmail.com>
2024-08-10 20:25:43 +0000

    AM-16 Setup project - Add devcontainer setup - Add initial .NET solution

---
+++ AssetsManagerApi.Api/Controllers/ApiController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetsManagerApi.Api.Controllers;
+
+[ApiController]
+public class ApiController : ControllerBase
+{
+    
+}
---
+++ AssetsManagerApi.Api/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+builder.Services
+    .AddCors(options =>
+        {
+            options.AddPolicy("allowAnyOrigin",
+                builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+        });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("allowAnyOrigin");
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+public partial class Program {}
