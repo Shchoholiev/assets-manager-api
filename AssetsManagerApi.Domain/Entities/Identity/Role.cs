8d1cebebc8dd746496b99f094d392b97efc13599
Shchoholiev <sh.codework@gmail.com>
2024-08-11 20:06:37 +0000

    AM-6 Add entities

---
+++ AssetsManagerApi.Domain/Entities/Identity/Role.cs
@@ -0,0 +1,6 @@
+﻿namespace AssetsManagerApi.Domain.Entities.Identity;
+
+public class Role : EntityBase
+{
+    public string Name { get; set; }
+}
---
+++ AssetsManagerApi.Domain/Entities/Identity/User.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+
+namespace AssetsManagerApi.Domain.Entities.Identity;
+
+public class User : EntityBase
+{
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? PasswordHash { get; set; }
+
+    public ObjectId? CompanyId { get; set; }
+
+    public List<Role> Roles { get; set; }
+}
