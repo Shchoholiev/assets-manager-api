f3064cc3ed2b77d4d7aee53a58c0e6ef5cde07cd
Mykhailo-Bilodid <m.bilodid@opflo.com>
2025-01-27 22:17:40 +0200

    AM-51 Serialization fixed

--- AssetsManagerApi.Application/Models/Dto/FileSystemNodeDto.cs
+++ AssetsManagerApi.Application/Models/Dto/FileSystemNodeDto.cs
@@ -1,8 +1,11 @@
 ﻿using AssetsManagerApi.Domain.Entities;
 using AssetsManagerApi.Domain.Enums;
+using System.Text.Json.Serialization;
 
 namespace AssetsManagerApi.Application.Models.Dto;
 
+[JsonDerivedType(typeof(FolderDto), "folder")]
+[JsonDerivedType(typeof(CodeFileDto), "codeFile")]
 public class FileSystemNodeDto
 {
     public string Id { get; set; }
